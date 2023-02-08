f62fc4c828de0f5c9f561154c4b0276f8d57c406
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-02-08 14:52:47 -0800

    Prototype Bump allocator

--- include/libdmt/allocator/bump.hpp
+++ include/libdmt/allocator/bump.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <array>
 #include <cstdlib>
 
 namespace dmt {
 namespace allocator {
-template <class T> class Bump {
+
+static constexpr std::size_t kDefaultStorageSize = 4096;
+
+// TODO: Lazy v. Eager Allocation
+// TODO: Static Buffer vs Heap
+template <class T, std::size_t StorageSize = kDefaultStorageSize> class Bump {
 public:
   // Require alias for std::allocator_traits to infer other types, e.g.
   // using pointer = value_type*.
@@ -12,11 +18,51 @@ public:
 
   explicit Bump(){};
 
+  ~Bump() {
+    if (storage_)
+      std::free(storage_);
+  }
+
   template <class U> constexpr Bump(const Bump<U> &) noexcept {}
 
-  T *allocate(std::size_t n) { return static_cast<T *>(std::malloc(n)); }
+  T *allocate(std::size_t n) {
+    if (n > StorageSize)
+      return nullptr;
 
-  void deallocate(T *p, std::size_t n) { std::free(p); }
+    if (!storage_) {
+      if (storage_ = static_cast<Byte *>(std::malloc(StorageSize)); !storage_) {
+        return nullptr;
+      }
+    }
+
+    size_t request_size = AlignUp(n);
+    size_t remaining_size = StorageSize - offset;
+
+    if (request_size > remaining_size)
+      return nullptr;
+
+    Byte *result = storage_ + offset;
+    offset += request_size;
+
+    return reinterpret_cast<T *>(result);
+  }
+
+  void deallocate(T *p, std::size_t n) { /* no op */
+  }
+
+private:
+  using Byte = uint8_t;
+
+  std::size_t AlignUp(std::size_t n, std::size_t alignment = 2) {
+    if (n < alignment)
+      return alignment;
+
+    std::size_t remainder = n % alignment;
+    return remainder ? n + (alignment - remainder) : n;
+  }
+
+  size_t offset = 0;
+  Byte *storage_ = nullptr;
 };
 
 template <class T, class U> bool operator==(const Bump<T> &, const Bump<U> &) {
--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,18 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
 
-TEST_CASE("bump") {
-  REQUIRE(1 == 1);
-  REQUIRE(1 == 2);
+TEST_CASE("allocator::Bump") {
+  dmt::allocator::Bump<int> allocator;
+
+  int *a = allocator.allocate(sizeof(int));
+  REQUIRE(a != nullptr);
+
+  *a = 100;
+  allocator.deallocate(a, sizeof(int));
+  REQUIRE(*a == 100);
+
+  int *b = allocator.allocate(sizeof(int));
+  REQUIRE(b != nullptr);
+  REQUIRE(*a == 100);
+  REQUIRE(a + sizeof(int) == b);
 }
