f0e13c654b429877c85b395f74285b25727ee270
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-02-08 19:34:23 -0800

    Switch test to BDD style

--- test/test.cpp
+++ test/test.cpp
@@ -2,18 +2,33 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
 
-TEST_CASE("allocator::Bump") {
-  dmt::allocator::Bump<int> allocator;
+SCENARIO("Bump allocator can allocate objects", "[allocator::Bump]") {
+  GIVEN("an integer allocator with a size of sizeof(int) * 2") {
+    dmt::allocator::Bump<
+        /*T=*/int,
+        /*StorageSize=*/sizeof(int) * 2>
+        allocator;
 
-  int *a = allocator.allocate(sizeof(int));
-  REQUIRE(a != nullptr);
+    int *a = allocator.allocate(sizeof(int));
+    WHEN("an object (within size) is allocated") {
+      THEN("it is given a valid pointer address") { REQUIRE(a != nullptr); }
+    }
 
-  *a = 100;
-  allocator.deallocate(a, sizeof(int));
-  REQUIRE(*a == 100);
+    WHEN("another object is allocated") {
+      int *b = allocator.allocate(sizeof(int));
+      REQUIRE(b != nullptr);
 
-  int *b = allocator.allocate(sizeof(int));
-  REQUIRE(b != nullptr);
-  REQUIRE(*a == 100);
-  REQUIRE(a + 1 == b);
+      THEN("it is set to the address next to the previously allocated one") {
+        REQUIRE(a + 1 == b);
+      }
+    }
+
+    WHEN("deallocate is invoked") {
+      THEN("the allocated objects remain valid") {
+        *a = 100;
+        allocator.deallocate(a, sizeof(int));
+        REQUIRE(*a == 100);
+      }
+    }
+  }
 }
