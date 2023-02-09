cfb92b0435a76f8c632ba122f4643d168433f3e7
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-02-09 15:05:33 -0800

    Change test to use long instead of int
    The former's sizeof and alignment is greater
    than `sizeof(void*)`, thus allowing the happy
    path tests to stay the same w/o special casing.

--- test/test.cpp
+++ test/test.cpp
@@ -4,18 +4,21 @@
 
 SCENARIO("Bump allocator can allocate objects", "[allocator::Bump]") {
   GIVEN("an integer allocator with a size of sizeof(int) * 2") {
+    using T = long;
+    static constexpr std::size_t SizeOfT = sizeof(T);
+
     using Allocator =
-        dmt::allocator::Bump</*T=*/int, /*StorageSize=*/sizeof(int) * 2>;
+        dmt::allocator::Bump<T, /*StorageSize=*/SizeOfT * 2>;
     Allocator allocator;
 
-    int* a = allocator.allocate(sizeof(int));
+    T* a = allocator.allocate(SizeOfT);
     WHEN("an object (within size) is allocated") {
       THEN("it is given a valid pointer address") { REQUIRE(a != nullptr); }
     }
 
     WHEN("another object is allocated") {
       REQUIRE(a != nullptr);
-      int* b = allocator.allocate(sizeof(int));
+      T* b = allocator.allocate(SizeOfT);
       REQUIRE(b != nullptr);
 
       THEN("it is set to the address next to the previously allocated one") {
@@ -27,15 +30,15 @@ SCENARIO("Bump allocator can allocate objects", "[allocator::Bump]") {
       REQUIRE(a != nullptr);
       THEN("the allocated objects remain valid") {
         *a = 100;
-        allocator.deallocate(a, sizeof(int));
+        allocator.deallocate(a, SizeOfT);
         REQUIRE(*a == 100);
       }
     }
 
     WHEN("allocate is invoked over capacity") {
-      REQUIRE(allocator.allocate(sizeof(int)) != nullptr);
+      REQUIRE(allocator.allocate(SizeOfT) != nullptr);
 
-      REQUIRE(allocator.allocate(sizeof(int)) == nullptr);
+      REQUIRE(allocator.allocate(SizeOfT) == nullptr);
     }
   }
 }
