3f4eeda94f8d1f5be07880026aa1a274307bb161
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-02-12 20:54:30 -0800

    Add basic gist of growth-based strategy

--- include/libdmt/allocator/bump.hpp
+++ include/libdmt/allocator/bump.hpp
@@ -23,6 +23,12 @@ struct AlignmentT : std::integral_constant<std::size_t, Alignment> {
   using Id_ = struct {};
 };
 
+enum WhenFull { ReturnNull = 0, GrowStorage = 1 };
+
+template <WhenFull WF> struct GrowT : std::integral_constant<WhenFull, WF> {
+  using Id_ = struct {};
+};
+
 // TODO: Arena allocations when at capacity and using heap
 template <class T, typename... Args> class Bump {
 public:
@@ -40,19 +46,20 @@ public:
     if (n > AlignedSize_)
       return nullptr;
 
+    if (!chunks_)
+      if (chunks_ = AllocateNewChunk(); !chunks_)
+        return nullptr;
+
     size_t request_size = internal::AlignUp(n, Alignment_);
     size_t remaining_size = AlignedSize_ - offset_;
 
-    if (request_size > remaining_size)
+    if (request_size > remaining_size) {
+      // TODO: Get new chunk
       return nullptr;
-
-    if (!chunk_.has_value()) {
-      chunk_ = internal::AllocateBytes(AlignedSize_, Alignment_);
-      if (!chunk_.has_value())
-        return nullptr;
     }
 
-    Byte* result = chunk_->base + offset_;
+    Byte* base = ChunkHeader::GetChunk(chunks_);
+    Byte* result = base + offset_;
     offset_ += request_size;
 
     return reinterpret_cast<T*>(result);
@@ -64,13 +71,25 @@ public:
 
   void Reset() {
     offset_ = 0;
-    if (chunk_.has_value())
-      internal::ReleaseBytes(chunk_.value());
-    chunk_ = std::nullopt;
+    if (chunks_)
+      ReleaseChunk(chunks_);
+    chunks_ = nullptr;
   }
 
 private:
   using Byte = uint8_t;
+
+  struct ChunkHeader {
+    ChunkHeader* next = nullptr;
+
+    static constexpr std::size_t GetChunkHeaderSize() {
+      return sizeof(ChunkHeader);
+    }
+
+    static Byte* GetChunk(ChunkHeader* header) {
+      return reinterpret_cast<Byte*>(header) + sizeof(header->next);
+    }
+  };
 
   // There are several factors used to determine the alignment for the
   // allocator. First, users can specify their own alignment if desired using
@@ -84,11 +103,56 @@ private:
   static_assert(internal::IsPowerOfTwo(Alignment_),
                 "Alignment must be a power of 2.");
 
+  static constexpr std::size_t RequestSize_ =
+      internal::GetValueT<SizeT<kDefaultSize>, Args...>::value;
+
   static constexpr std::size_t AlignedSize_ = internal::AlignUp(
-      internal::GetValueT<SizeT<kDefaultSize>, Args...>::value, Alignment_);
+      RequestSize_ + ChunkHeader::GetChunkHeaderSize(), Alignment_);
+
+  static constexpr bool GrowWhenFull_ =
+      internal::GetValueT<GrowT<WhenFull::GrowStorage>, Args...>::value ==
+      WhenFull::GrowStorage;
+
+  static internal::Allocation CreateAllocation(Byte* base) {
+    std::size_t size = IsPageMultiple() ? AlignedSize_ / internal::GetPageSize()
+                                        : AlignedSize_;
+    return internal::Allocation{.base = static_cast<internal::Byte*>(base),
+                                .size = size};
+  }
+
+  static bool IsPageMultiple() {
+    static const auto page_size = internal::GetPageSize();
+    return AlignedSize_ > page_size && AlignedSize_ % page_size == 0;
+  }
+
+  static ChunkHeader* AllocateNewChunk() {
+    auto allocation =
+        IsPageMultiple()
+            ? internal::AllocatePages(AlignedSize_ / internal::GetPageSize())
+            : internal::AllocateBytes(AlignedSize_, Alignment_);
+
+    if (!allocation.has_value())
+      return nullptr;
+
+    Byte* base = allocation.value().base;
+    memset(static_cast<void*>(base), 0, allocation.value().size);
+    ChunkHeader* header =
+        reinterpret_cast<ChunkHeader*>(allocation.value().base);
+    header->next = nullptr;
+    return header;
+  }
+
+  static void ReleaseChunk(ChunkHeader* chunk) {
+    auto allocation = CreateAllocation(reinterpret_cast<Byte*>(chunk));
+    if (IsPageMultiple()) {
+      internal::ReleasePages(allocation);
+    } else {
+      internal::ReleaseBytes(allocation);
+    }
+  }
 
   size_t offset_ = 0;
-  std::optional<internal::Allocation> chunk_ = std::nullopt;
+  ChunkHeader* chunks_ = nullptr;
 };
 
 template <class T, class U> bool operator==(const Bump<T>&, const Bump<U>&) {
