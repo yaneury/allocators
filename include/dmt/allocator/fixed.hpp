99aab4ca8a7571a479b2b8722f8529c508a35f4a
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-03-20 08:10:23 -0700

    Sketch out fixed allocator

---
+++ include/dmt/allocator/fixed.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <array>
+
+#include <dmt/allocator/error.hpp>
+#include <dmt/allocator/parameters.hpp>
+#include <dmt/allocator/trait.hpp>
+
+namespace dmt::allocator {
+
+template <class... Args> class Fixed {
+public:
+  // Size of the blocks. This allocator doesn't support variable-sized blocks.
+  // All blocks allocated are of the same size. N.b. that the size here will
+  // *not* be the size of memory ultimately requested for blocks. This is so
+  // because supplemental memory is needed for block headers and to ensure
+  // alignment as specified with |kAlignment|.
+  //
+  // This field is optional. If not provided, will default to the common page
+  // size, 4096.
+  static constexpr std::size_t kSize =
+      ntp::optional<SizeT<4096>, Args...>::value;
+
+  Result<std::byte*> AllocateUnaligned(std::size_t size) {
+    // This allocator does not enforce alignment.
+    return Allocate(Layout{.size = size, .alignment = 1});
+  }
+
+  Result<std::byte*> Allocate(Layout layout) {
+    if (layout.size == 0)
+      return cpp::fail(Error::InvaldInput);
+
+    std::size_t space_remaining = kSize - end_;
+    if (layout.size > space_remaining)
+      return cpp::fail(Error::ReachedMemoryLimit);
+
+    std::byte* ptr = buffer_[end_];
+    end_ += layout.size;
+    return ptr;
+  }
+
+  Result<void> Release(std::byte* ptr) { return {}; }
+
+private:
+  std::array<std::byte, kSize> buffer_;
+  std::size_t end_ = 0;
+};
+
+} // namespace dmt::allocator
