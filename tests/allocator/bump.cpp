56e8b3c5dfbd2e14d8165e0d0c324f7afcb20388
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-03-29 12:27:56 -0700

    Improve Bump allocator tests

--- tests/allocator/bump.cpp
+++ tests/allocator/bump.cpp
@@ -3,94 +3,95 @@
 #include <dmt/allocator/adapter.hpp>
 #include <dmt/allocator/bump.hpp>
 
+#include "catch2/catch_test_macros.hpp"
 #include "util.hpp"
 
 using namespace dmt::allocator;
 
-TEST_CASE("Bump allocator", "[allocator][Bump]") {
-  using T = long;
-  static constexpr std::size_t SizeOfT = sizeof(T);
+using T = long;
+static constexpr std::size_t SizeOfT = sizeof(T);
+static constexpr std::size_t N = 10;
+static constexpr std::size_t MinBlockSize = SizeOfT * N;
+static constexpr std::size_t MaxBlockSize =
+    MinBlockSize + internal::GetBlockHeaderSize();
 
-  SECTION("Fixed-sized allocator that can fit two objects") {
-    using Allocator = Bump<SizeT<SizeOfT * 2>, GrowT<WhenFull::ReturnNull>,
-                           LimitT<BlocksMust::HaveAtLeastSizeBytes>>;
-    Allocator allocator;
+template <class... Allocator> struct AllocatorPack {};
 
-    T* a = GetPtrOrFail<T>(allocator.Allocate(SizeOfT));
-    SECTION("Can allocate request within size") { REQUIRE(a != nullptr); }
+template <class... Args>
+using FixedBump = Bump<GrowT<WhenFull::ReturnNull>, Args...>;
 
-    SECTION("another object is allocated") {
-      REQUIRE(a != nullptr);
-      T* b = GetPtrOrFail<T>(allocator.Allocate(SizeOfT));
-      REQUIRE(b != nullptr);
+using FixedBumpAllocators = AllocatorPack<
+    FixedBump<LimitT<BlocksMust::HaveAtLeastSizeBytes>, SizeT<MinBlockSize>>,
+    FixedBump<LimitT<BlocksMust::NoMoreThanSizeBytes>, SizeT<MaxBlockSize>>>;
 
-      SECTION("it is set to the address next to the previously allocated one") {
-        REQUIRE(a + 1 == b);
-      }
-    }
+TEMPLATE_LIST_TEST_CASE("Fixed Bump allocator that can fit N objects",
+                        "[allocator][Bump][fixed]", FixedBumpAllocators) {
+  TestType allocator;
 
-    SECTION("deallocate is invoked") {
-      REQUIRE(a != nullptr);
-      SECTION("the allocated objects remain valid") {
-        *a = 100;
-        REQUIRE(allocator.Release(ToBytePtr(a)) ==
-                cpp::fail(Error::OperationNotSupported));
-        REQUIRE(*a == 100);
-      }
-    }
-    SECTION("allocate is invoked over capacity") {
-      REQUIRE(allocator.Allocate(SizeOfT) != nullptr);
+  std::array<T*, N> allocs;
+  for (std::size_t i = 0; i < N; ++i)
+    allocs[i] = GetPtrOrFail<T>(allocator.Allocate(SizeOfT));
 
-      SECTION("allocate returns nullptr") {
-        REQUIRE(allocator.Allocate(SizeOfT) ==
-                cpp::fail(Error::ReachedMemoryLimit));
-      }
-    }
+  SECTION("All objects are neighbors to each other") {
+    for (std::size_t i = 0; i < N - 1; ++i)
+      REQUIRE(allocs[i] + 1 == allocs[i + 1]);
   }
 
-  SECTION("a variable-sized allocator that can fit two objects") {
-    using Allocator = Bump<SizeT<SizeOfT * 2>, GrowT<WhenFull::GrowStorage>>;
-    Allocator allocator;
-
-    SECTION("making more allocation than can fit in one block") {
-      SECTION("will grow until OOM") {
-        for (size_t i = 0; i < 100; ++i)
-          REQUIRE(allocator.Allocate(SizeOfT) != nullptr);
-      }
-    }
+  SECTION("Can not allocate more objects when at capacity") {
+    REQUIRE(allocator.Allocate(SizeOfT) ==
+            cpp::fail(Error::ReachedMemoryLimit));
   }
 
-  SECTION("pb", "a page-sized, page-aligned allocator with growth storage") {
-    static constexpr std::size_t PageSize = 4096;
-    using Allocator =
-        Bump<SizeT<PageSize - internal::GetBlockHeaderSize()>,
-             AlignmentT<PageSize>, LimitT<BlocksMust::HaveAtLeastSizeBytes>,
-             GrowT<WhenFull::GrowStorage>>;
-    Allocator allocator;
+  SECTION("Release is not supported") {
+    REQUIRE(allocator.Release(ToBytePtr(allocs.front())) ==
+            cpp::fail(Error::OperationNotSupported));
+  }
 
-    SECTION("making an allocation within page") {
-      SECTION("it allocates") {
-        T* a =
-            reinterpret_cast<T*>(GetValueOrFail(allocator.Allocate(SizeOfT)));
-        REQUIRE(a != nullptr);
-      }
-    }
+  SECTION("Reset clears space") {
+    REQUIRE(allocator.Reset().has_value());
 
-    // When accounting for header, page size should be larger than size of
-    // block.
-    SECTION("making an allocation greater than block size") {
-      SECTION("it returns error") {
-        REQUIRE(allocator.Allocate(PageSize) ==
-                cpp::fail(Error::SizeRequestTooLarge));
-      }
+    SECTION("Allowing subsequent requests") {
+      for (std::size_t i = 0; i < N; ++i)
+        allocs[i] = GetPtrOrFail<T>(allocator.Allocate(SizeOfT));
     }
   }
 }
 
-TEST_CASE("BumpAdapter allocator", "[allocator::BumpAdapter]") {
-  using T = long;
+template <class... Args>
+using VariableBump = Bump<GrowT<WhenFull::GrowStorage>, Args...>;
 
-  SECTION("a fixed-sized allocator that can hold a page worth of objects") {
+using VariableBumpAllocators = AllocatorPack<
+    VariableBump<LimitT<BlocksMust::HaveAtLeastSizeBytes>, SizeT<MinBlockSize>>,
+    VariableBump<LimitT<BlocksMust::NoMoreThanSizeBytes>, SizeT<MaxBlockSize>>>;
+
+TEMPLATE_LIST_TEST_CASE(
+    "Variable-sized Bump allocator with block size fitting N objects",
+    "[allocator][Bump][variable]", VariableBumpAllocators) {
+  TestType allocator;
+
+  for (std::size_t i = 0; i < N; ++i)
+    REQUIRE(allocator.Allocate(SizeOfT).has_value());
+
+  SECTION("Can make allocations beyond single block") {
+    // After this second loop, there should be two blocks filled with N
+    // allocations each.
+    for (std::size_t i = 0; i < N; ++i)
+      REQUIRE(allocator.Allocate(SizeOfT).has_value());
+  }
+
+  SECTION("Can reset allocations") {
+    REQUIRE(allocator.Reset().has_value());
+
+    SECTION("But can't fit request size larger than block size") {
+      REQUIRE(allocator.Allocate(MinBlockSize + 1) ==
+              cpp::fail(Error::SizeRequestTooLarge));
+    }
+  }
+}
+
+TEST_CASE("BumpAdapter allocator works with standard containers",
+          "[allocator][BumpAdapter]") {
+  SECTION("A fixed-sized allocator that can hold a page worth of objects") {
     static constexpr std::size_t PageSize = 4096;
     using Allocator = BumpAdapter<T, SizeT<PageSize>>;
 
@@ -98,6 +99,6 @@ TEST_CASE("BumpAdapter allocator", "[allocator::BumpAdapter]") {
     for (size_t i = 0; i < 100; ++i)
       values.push_back(i);
 
-    REQUIRE(true); // Should not panic here.
+    SUCCEED(); // Should not panic here.
   }
 }
