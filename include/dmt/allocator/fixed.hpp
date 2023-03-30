9ba41a9e14a31c5a69348b2942be9528a45bc1e1
Yaneury Fermin <fermin.yaneury@gmail.com>
2023-03-30 11:33:06 -0700

    Add comment for Fixed allocator

--- include/dmt/allocator/fixed.hpp
+++ include/dmt/allocator/fixed.hpp
@@ -12,14 +12,8 @@ namespace dmt::allocator {
 
 template <class... Args> class Fixed {
 public:
-  // Size of the blocks. This allocator doesn't support variable-sized blocks.
-  // All blocks allocated are of the same size. N.b. that the size here will
-  // *not* be the size of memory ultimately requested for blocks. This is so
-  // because supplemental memory is needed for block headers and to ensure
-  // alignment as specified with |kAlignment|.
-  //
-  // This field is optional. If not provided, will default to the common page
-  // size, 4096.
+  // Size of the memory block. This *must* match the size of the buffer passed
+  // to the object constructor.
   static constexpr std::size_t kSize =
       ntp::optional<SizeT<4096>, Args...>::value;
 
