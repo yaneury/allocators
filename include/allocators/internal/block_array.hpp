00d233ee2b524624bd5682fba5097901046517bc
Yaneury Fermin <fermin.yaneury@gmail.com>
2024-03-13 21:13:25 -0700

    feat: introduce BlockArray class
    This class will store page span entries for Page provider
    classes. It allows for quick insertions, O(1), and but
    slow removals in worst case O(N). Note, re-arranging
    elements in random-access removal is fast as we can
    swap with the last element in array. This is so because
    we do not care about the order of the elements

---
+++ include/allocators/internal/block_array.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <cstdint>
+#include <cstring>
+#include <utility>
+
+#include <allocators/internal/util.hpp>
+
+namespace allocators::internal {
+
+struct BlockArrayHeader {
+  std::size_t size;
+  std::uintptr_t next;
+};
+
+template <std::size_t Size, class T> class BlockArray {
+public:
+  ALLOCATORS_NO_COPY_NO_MOVE_NO_DEFAULT(BlockArray);
+
+  bool IsFull() const { return header.size == kCapacity; }
+
+  bool IsEmpty() const { return header.size == 0; }
+
+  bool HasNext() const { return header.next != 0; }
+
+  std::size_t GetSize() const { return header.size; }
+
+  std::size_t GetCapacity() const { return kCapacity; }
+
+  BlockArray<Size, T>* GetNext() const {
+    return reinterpret_cast<BlockArray<Size, T>*>(header.next);
+  }
+
+  void PushBackUnchecked(T value) { entries[header.size++] = value; }
+
+  bool PushBack(T value) {
+    if (IsFull())
+      return false;
+
+    PushBackUnchecked(value);
+    return true;
+  }
+
+  T PopBackUnchecked() { return entries[--header.size]; }
+
+  bool Remove(T target) {
+    for (auto i = 0; i < header.size; ++i) {
+      if (entries[i] == target) {
+        if (i != header.size - 1)
+          std::swap(entries[i], entries[header.size - 1]);
+        PopBackUnchecked();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  void SetNext(std::uintptr_t next) { header.next = next; }
+
+private:
+  static constexpr std::size_t kCapacity =
+      (Size - sizeof(BlockArrayHeader)) / AlignUp(sizeof(T), sizeof(void*));
+
+  BlockArrayHeader header;
+  T entries[kCapacity];
+};
+
+template <std::size_t Size, class T>
+BlockArray<Size, T>* AsBlockArrayPtr(std::byte* block, bool zero_out = true) {
+  if (zero_out)
+    bzero(block, Size);
+  return reinterpret_cast<BlockArray<Size, T>*>(block);
+}
+
+} // namespace allocators::internal
